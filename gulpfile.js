acc5bee6cb3f9c7d005889edc7cfae0d5fd58f65
Gnoush <agnes.guinin@gmail.com>
2018-06-30 00:36:25 +0200

    initial commiinitial commiinitial committt

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+/ DEPENDENCIES =================================================================================
+// PACKAGES ------------------------------------------------------------------------------------
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-minify-css');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const replace = require('gulp-replace');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+
+
+const path = {
+    css: './css/*.scss'
+};
+
+const js_path = [
+    './js/*.js'
+];
+
+// TASKS ========================================================================================
+// DEFAULT --------------------------------------------------------------------------------------
+/**
+ * Tasks who can be run
+ * 
+ * default : run for local watch
+ */
+gulp.task('default', () => {
+    gulp.start('watch');
+});
+
+// WATCH ----------------------------------------------------------------------------------------
+/**
+ * Minify and compile assets, and then launch browserSync
+ */
+gulp.task('watch', function () {
+    watcher();
+});
+
+/**
+ * Browser_Sync restart after change
+ */
+function watcher() {
+    browserSync.init({
+        server: dist,
+        browser: "google chrome"
+    });
+
+    watch(["./*.scss"], function () {
+        minify_css();
+        browserSync.reload();
+    });
+
+    watch("./*.js", () => {
+        minify_js();
+        browserSync.reload();
+    });
+}
+
+// ASSETS MINIFICATION --------------------------------------------------------------------------
+/**
+ * reduce CSS sources and librairies into a single minified CSS file
+ *
+ * @returns {*|void} result of Gulp data Stream
+ */
+function minify_css() {
+    return gulp.src([path.css])
+        .pipe(sass())
+        .pipe(concat('custom.min.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(`./`));
+}
+
+/**
+ * reduce JS sources and librairies into a single minified script file
+ *
+ * @returns {*|void} result of Gulp data Stream
+ */
+function minify_js() {
+    return gulp.src(js_path)
+        .pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(`${dist}/assets/js`));
+}
---
+++ index.php
@@ -0,0 +1,76 @@
+<nav>
+    <img class="logo-nav" src="./img/logo-ag.svg">
+    <div>
+        <a href="">projets</a>
+        <a href="">à propos</a>
+        <a href="">contact</a>
+        <a href="">cv</a>
+    </div>
+</nav>
+
+<section class="home">
+    <a class="apropos-container" href="#aboutme">
+        <img src="">
+        <h4 class="nav-title">à propos<span>de moi</span></h4>
+    </a>
+    <div class="main-part">
+        <a class="cv-container" href="">
+            <img src="">
+            <h4 class="nav-title"><span>Télécharger mon</span>CV</h4>
+        </a>
+        <div>
+            <img class="logo-opacity" src="">
+            <div class="name-container">
+                <h1>Agnès Guinin</h1>
+                <p>Webdesigner, Créatrice d'ambiance</p>
+            </div>
+        </div>
+        <a class="projets-container" href="#projects">
+            <img src="">
+            <h4 class="nav-title"><span>Mes</span>Projets</h4>
+        </a>
+    </div>
+    <a class="contact-container" href="#contact">
+        <img src="">
+        <h4 class="nav-title"><span>Me</span>Contacter</h4>
+    </a>
+</section>
+
+<section class="about-me" id="aboutme">
+    <div>
+        <img src="" alt="">
+    </div>
+    <div class="about-me-description">
+        <h2>à propos <span>de moi</span></h2>
+        <p>blablabla description</p>
+    </div>
+</section>
+
+<section class="projects-main-container" id="projects">
+    <h2>Projets</h2>
+    <div class="projects-container">
+        <a class="projects" href="">
+            <div class="project-description">
+                <h3>Je suis un projet</h3>
+                <p>Je suis une description de projet</p>
+            </div>
+            <div class="img-shadow-container">
+                <img src="">
+            </div>
+        </a>
+        <a class="projects" href="">
+            <div class="project-description">
+                <h3>Je suis un projet</h3>
+                <p>Je suis une description de projet</p>
+            </div>
+            <div class="img-shadow-container">
+                <img src="">
+            </div>
+        </a>
+    </div>
+</section>
+
+<form id="contact">
+    <h2>Contact</h2>
+    <input>
+</form>

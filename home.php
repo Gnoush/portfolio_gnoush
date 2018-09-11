4c7d731e157db708c5ddf6c58655785180b18fd6
Gnoush <agnes.guinin@gmail.com>
2018-09-11 11:30:10 +0200

    responsive page projet + footer

--- home.php
+++ home.php
@@ -269,6 +269,11 @@
         </form>
     </section>
 
+    <footer>
+        <div class="footer-line"></div>
+        <p>copyright Agnès Guinin ©</p>
+    </footer>
+
     <script id="__bs_script__">//<![CDATA[
         document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.24.5'><\/script>".replace("HOST", location.hostname));
         //]]></script>

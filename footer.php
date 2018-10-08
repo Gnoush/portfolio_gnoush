5676b9e042664e38597bd332a70a75be99a333e1
ChazalFlorian <chazal.florian@gmail.com>
2018-10-09 01:37:54 +0200

    Multiples improvements: - refactor projects template by refactoring header and footer blocks - add contact icon and href - init js variables that  will be used in main.js

---
+++ footer.php
@@ -0,0 +1,31 @@
+<section class="direction">
+    <a href="" class="fas fa-angle-left  arrow-project-nav-left"></a>
+    <a href="" class="fas fa-angle-right  arrow-project-nav-right"></a>
+</section>
+
+<footer>
+    <div class="footer-line"></div>
+    <p>copyright Agnès Guinin ©</p>
+</footer>
+
+<script type="text/javascript" src="<?php echo get_bloginfo('stylesheet_directory') ?>/script.min.js"></script>
+<script type="text/javascript">
+    //Shamelessly stole data returned from php
+
+    /**
+     * Store the Current theme's path
+     *
+     * @type {string}
+     */
+    var stylesheetDirectory = "<?php echo get_bloginfo('stylesheet_directory') ?>";
+
+    var permalink = "<?php echo get_site_url();?>";
+</script>
+<script type="text/javascript">
+    initProjectDisplay();
+</script>
+<script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.24.5'><\/script>".replace("HOST", location.hostname));
+    //]]></script>
+</body>
+</html>
--- stayinshape.php
+++ stayinshape.php
@@ -1,29 +1,5 @@
 <?php /* Template Name: stayshape */ ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('stylesheet_directory'); ?>/style.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/brands.css" integrity="sha384-nT8r1Kzllf71iZl81CdFzObMsaLOhqBU1JD2+XoAALbdtWaXDOlWOZTR4v1ktjPE" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css" integrity="sha384-HbmWTHay9psM8qyzEKPc8odH4DsOuzdejtnr+OFtDmOcIVnhgReQ4GZBH7uwcjf6" crossorigin="anonymous">
-</head>
-<body>
-    <header>
-            <a class="logo-home" href="<?php echo esc_url( home_url('/') ); ?>">
-                <img class="logo-projects" src="<?php echo get_bloginfo('stylesheet_directory'); ?>/img/logo-ag.svg">
-                <div class="nav-name">
-                    <h3>Agnès Guinin</h3>
-                    <p>Webdesigner</p>
-                </div>
-            </a>
-            <a href="" class="far fa-paper-plane contact-cta">
-                Contact
-            </a>
-    </header>
-
-    <section class="direction">
-        <a href="" class="fas fa-angle-left"></a>
-        <a href="" class="fas fa-angle-right"></a>
-    </section>
+<?php include_once "header.php"; ?>
 
     <section class="project-name">
         <h1>Projet Stay in Shape</h1>
@@ -38,18 +14,4 @@
         <img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/img/projets-details/stay-in-shape/sis.png" alt="">
     </section>
 
-    <section class="direction">
-        <a href="" class="fas fa-angle-left"></a>
-        <a href="" class="fas fa-angle-right"></a>
-    </section>
-
-    <footer>
-        <div class="footer-line"></div>
-        <p>copyright Agnès Guinin ©</p>
-    </footer>
-
-    <script id="__bs_script__">//<![CDATA[
-        document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.24.5'><\/script>".replace("HOST", location.hostname));
-        //]]></script>
-</body>
-</html>
+<?php include_once "footer.php"; ?>

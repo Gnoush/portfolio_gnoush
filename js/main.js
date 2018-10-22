cbe1a5d03773a970bda9c365ada13652ba72f2c4
ChazalFlorian <chazal.florian@gmail.com>
2018-10-23 00:59:09 +0200

    feat: implement dynamic navigation between pages when an user click, on a project page, on the nav arrows, the previous or next project are loaded and replace the page content     it make the projects consultation smooth and comfortable for the user also add comments

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@
 /**
  * JSON Object containing all of Gnoush projects
  *
+ * Is populated at runtime when a projects page is displayed
+ *
+ * contains JSON objects, of the current format:
+ * "projectName": {
+ *     "isDisplayed": @type {Boolean},
+ *     "templateName": @type {String},
+ *     "url": @type {String}
+ * }
+ *
  * @type {JSON}
  */
 var projects = [];
@@ -9,12 +18,14 @@ var projects = [];
 /**
  * ordered Array which contains index of all JSON objects in projects
  *
+ * Is populated at runtime when a projects page is displayed
+ *
  * @type {string[]}
  */
 var projectKeys = [];
 
 /**
- * represent the current place of the displayed template, in projects
+ * index of the currently displayed project in ordered array projectKeys
  *
  * @type {number}
  */
@@ -22,13 +33,16 @@ var loc = 0;
 
 /////////////////////////////////FUNCTIONS
 /**
- * Initialize the Triggers, which will allw the user clicking it to navigate within project
+ * Initialize the dynamic navigation between projects
+ *
+ * On projects pages, two CTA arrows allow to dynamically load the previous or next project,
+ * and replace the current page content with the retrieved datas
+ *
+ * it flag the current displayed project, and set the onClick Event Listeners on the CTAs
  */
-function initProjectDisplay() {
-    //get Current template and actualize array of template's reference
+function initDynamicNavigationBetweenProjects() {
     projects[currentTemplate].isDisplayed = true;
 
-    //set OnClickEventListener
     $(".arrow-icon-container.arrow-project-nav-left").click(function (e) {
         e.preventDefault();
         setPreviousProject();
@@ -42,7 +56,7 @@ function initProjectDisplay() {
 
 /**
  * If for any obscure reason the projects resource file cannot be loaded,
- * add these dummies links to avoid non-working buttons
+ * add these dummies links to avoid useless buttons
  */
 function setNavigationHrefFallback() {
     $(".arrow-project-nav-left").attr("href", permalink + "/dumbo/");
@@ -50,7 +64,11 @@ function setNavigationHrefFallback() {
 }
 
 /**
+ * Change the appropriate values and load the selected template when clicking the Previous Arrow
  *
+ * When clicking the Previous CTA, set the current project display status to false,
+ * get the previous project in projects (or the last one if currently displayed was the first one),
+ * and initiate the newly selected template loading
  */
 function setPreviousProject() {
     projects[currentTemplate].isDisplayed = false;
@@ -60,10 +78,15 @@ function setPreviousProject() {
 
     projects[currentTemplate].isDisplayed = true;
     loc = projectKeys.indexOf(projects[currentTemplate].templateName);
+    loadProjectPage();
 }
 
 /**
+ * Change the appropriate values and load the selected template when clicking the Next Arrow
  *
+ * When clicking the Next CTA, set the current project display status to false,
+ * get the next project in projects (or the first one if currently displayed was the last one),
+ * and initiate the newly selected template loading
  */
 function setNextProject() {
     projects[currentTemplate].isDisplayed = false;
@@ -73,17 +96,52 @@ function setNextProject() {
 
     projects[currentTemplate].isDisplayed = true;
     loc = projectKeys.indexOf(projects[currentTemplate].templateName);
+    loadProjectPage();
 }
 
 /**
+ * Load the currently selected project's template and replace the actual window content with it
  *
+ * when called, will execute an asynchronous request to fetch the currently displayed project's template
+ *     - If the response is an error, log it and do nothing.
+ *     - If the response is a success, replace the page content (between header and footer) by the requested template
+ */
+function loadProjectPage() {
+    $.ajax(stylesheetDirectory + "/html/" + projects[currentTemplate].templateName + ".html", {
+        async: true,
+        type: "GET",
+        crossDomain: true,
+        error: function (jqXhr, message, error) {
+            console.log("Error: " + message);
+            console.log(error.toString());
+        },
+        success: function (data, status, jqXhr) {
+            targetedString = "<?php echo get_bloginfo('stylesheet_directory'); ?>";
+            regEx = new RegExp(targetedString.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&'), 'g');
+            content = data.replace(regEx, stylesheetDirectory);
+
+            $(".project-wrapper").remove();
+            $(".project-details").remove();
+            $("section.direction").first().after(content);
+        },
+        beforeSend: function (jqXhr, settings) {
+            //TODO: add Loaoer init
+        },
+        complete: function (jqXhr, status) {
+            //TODO: add Loaoer finish
+        }
+    })
+}
+
+/**
+ * Using the received parameters, build and send a mail to agnes.guinin@gmail.com
  *
- * @param name
- * @param companyName
- * @param status
- * @param email
- * @param phone
- * @param message
+ * @param name of the mail's sender
+ * @param companyName of the mail's sender
+ * @param status of the request
+ * @param email of the mail's sender
+ * @param phone of the mail's sender
+ * @param message attached to the mail
  * @returns {boolean}
  */
 function sendContactForm(name, companyName, status, email, phone, message) {
@@ -100,11 +158,13 @@ function sendContactForm(name, companyName, status, email, phone, message) {
         },
         dataType: "jsonp",
         crossDomain: true,
-        error: function () {
-            console.log("fail");
+        error: function (jqXhr, message, error) {
+            console.log("Error: " + message);
+            console.log(error.toString());
         },
-        success: function () {
-            console.log("win");
+        success: function (data, status, jqXhr) {
+            console.log(status);
+            console.log(data);
         }
     });
 
@@ -122,13 +182,13 @@ $(document).ready(function () {
                 projectKeys = Object.keys(projects).sort();
                 loc = projectKeys.indexOf(projects[currentTemplate].templateName);
 
-                initProjectDisplay();
+                initDynamicNavigationBetweenProjects();
             })
-            .fail(function (xhrRequest, textStatus, error) {
-                console.log("Error: " + textStatus);
+            .fail(function (jqXhr, message, error) {
+                console.log("Error: " + message);
                 console.log(error.toString());
 
-                setNavigationHrefFallback()
+                setNavigationHrefFallback();
             });
     } else {
         //on click on #contact_submit_action, init a mail reation using the Contact Form inputs' values
